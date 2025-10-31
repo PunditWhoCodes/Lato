9c84e10508035c7a31b033bafcb62840bb1f6a28
Nawal Rai Chetan <152277551+PunditWhoCodes@users.noreply.github.com>
2025-11-01 02:05:05 +0500

    Initial set-up

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Lato",
+  description: "Lato Frontend Application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={GeistSans.className}>{children}</body>
+    </html>
+  );
+}
