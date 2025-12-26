f044604e1f597866d19f236599736e7f8820185e
Nawal Rai Chetan <152277551+PunditWhoCodes@users.noreply.github.com>
2025-12-26 05:58:47 +0500

    feat: update next.config with image domains

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "lato.gumlet.io",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
     ],
   },
 };
